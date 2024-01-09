e78e9cd4ff854daf1b9d89da15b3f1692e48bd1b
Paul Rivière <paulriviere002@gmail.com>
2024-01-09 10:16:52 +0100

    Fix dracoloader glb error

--- frontend/src/script_modules/glbImport.js
+++ frontend/src/script_modules/glbImport.js
@@ -1,7 +1,11 @@
 import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
+// import {DRACOLoader} from 'three/addons/loaders/DRACOLoader.js';
 
 export const loadModel = (modelPath, callback) => {
     const loader = new GLTFLoader();
+    /*const dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath( '../decoder/' );
+    loader.setDRACOLoader( dracoLoader );*/
     loader.load(
         modelPath,
         function (gltf) {
